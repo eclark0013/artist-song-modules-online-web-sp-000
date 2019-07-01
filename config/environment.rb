82a50698597e8bef458273200f9b3c40f2e89664
Eric Clark <eclark0013@gmail.com>
2019-07-01 14:22:25 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,30 @@ require 'pry'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+module methods
+
+  module ClassMethods
+    def self.find_by_name(name)
+      @@all.detect{|a| a.name == name}
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.reset_all
+      self.all.clear
+    end
+
+    def self.count
+      self.all.count
+    end
+  end
+
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+end
